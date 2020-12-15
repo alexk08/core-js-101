5a394443842127e8a5172ddef9a01b59bcafde7a
Aleksandr <akapacuk@gmail.com>
2020-12-15 20:39:37 +0400

    feat: add solutions circle and rectanglein 8 block

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -130,8 +130,8 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  return (rect1.top + rect1.height > rect2.top && rect1.left + rect1.width > rect2.left);
 }
 
 
@@ -161,8 +161,9 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  return (Math.sqrt((point.x - circle.center.x) ** 2 + (point.y - circle.center.y) ** 2) < circle
+    .radius);
 }
 
 
@@ -274,21 +275,16 @@ function reverseInteger(num) {
 function isCreditCardNumber(ccn) {
   const ccnStr = ccn.toString();
   let sum = 0;
-
   for (let i = 0; i < ccnStr.length; i += 1) {
     let cardNum = parseInt(ccnStr[i], 10);
-
     if ((ccnStr.length - i) % 2 === 0) {
       cardNum *= 2;
-
       if (cardNum > 9) {
         cardNum -= 9;
       }
     }
-
     sum += cardNum;
   }
-
   return sum % 10 === 0;
 }
 
